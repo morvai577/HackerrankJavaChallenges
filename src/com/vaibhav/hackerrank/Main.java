49b1f221e267e9c204feb64c15c6abdd2737f2e9
Vai Mor <mor.vaibhav577@gmail.com>
2017-04-14 20:26:27 +1200

    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line.

--- src/com/vaibhav/hackerrank/Main.java
+++ src/com/vaibhav/hackerrank/Main.java
@@ -1,8 +1,18 @@
 package com.vaibhav.hackerrank;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan1 = new Scanner(System.in);
+
+        int a = scan1.nextInt();
+        int b = scan1.nextInt();
+        int c = scan1.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 }
