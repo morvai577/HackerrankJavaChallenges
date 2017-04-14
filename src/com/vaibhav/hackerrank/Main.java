164c29f4a993379ae4c69790f05ee2819524ef1b
Vai Mor <mor.vaibhav577@gmail.com>
2017-04-14 20:42:38 +1200

    Simple exercise which takes an input integer and prints a certain message depending on whether it's within a range and whether it's an odd or even number. This exercise requires you to demonstrate your understanding of conditional logic.

--- src/com/vaibhav/hackerrank/Main.java
+++ src/com/vaibhav/hackerrank/Main.java
@@ -7,12 +7,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan1 = new Scanner(System.in);
 
-        int a = scan1.nextInt();
-        int b = scan1.nextInt();
-        int c = scan1.nextInt();
+        int n = scan1.nextInt();
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
+        if (n % 2 != 0){
+            System.out.println("Weird");
+        }
+        else{
+            if (n >= 2 & n <= 5){
+                System.out.println("Not Weird");
+            }
+            else if (n >= 6 & n <= 20){
+                System.out.println("Weird");
+            }
+            else if (n > 20){
+                System.out.println("Not Weird");
+            }
+        }
     }
 }
