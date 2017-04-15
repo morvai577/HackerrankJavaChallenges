a78d9294c882d46ff551d21b91cff197f78e57ab
Zaengar <Anonymous>
2017-04-15 14:38:41 +1200

    In this challenge, you must read an integer, a double, and a String from stdin, then print the values according to the instructions provided.

--- src/com/vaibhav/hackerrank/Main.java
+++ src/com/vaibhav/hackerrank/Main.java
@@ -1,8 +1,22 @@
 package com.vaibhav.hackerrank;
 
+import java.util.Scanner;
+import java.io.BufferedReader;
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan1 = new Scanner(System.in);
+        Scanner scan2 = new Scanner(System.in);
+
+        int a = scan1.nextInt();
+        double b = scan1.nextDouble();
+
+        String c = scan2.nextLine(); // Have to create a new scanner
+
+        System.out.println("String: " + c);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
     }
 }
