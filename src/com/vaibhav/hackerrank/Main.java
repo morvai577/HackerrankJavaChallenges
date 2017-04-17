bd258f810a3acda6f8f16a7be2c5b4026c8c2eff
Vai Mor <mor.vaibhav577@gmail.com>
2017-04-18 09:48:34 +1200

    Java's System.out.printf function can be used to print formatted output. The purpose of this exercise is to test understanding of formatting output using printf.

--- src/com/vaibhav/hackerrank/Main.java
+++ src/com/vaibhav/hackerrank/Main.java
@@ -1,22 +1,18 @@
 package com.vaibhav.hackerrank;
 
 import java.util.Scanner;
-import java.io.BufferedReader;
-
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scan1 = new Scanner(System.in);
-        Scanner scan2 = new Scanner(System.in);
-
-        int a = scan1.nextInt();
-        double b = scan1.nextDouble();
-
-        String c = scan2.nextLine(); // Have to create a new scanner
-
-        System.out.println("String: " + c);
-        System.out.println("Double: " + b);
-        System.out.println("Int: " + a);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================================");
+        for(int i = 0; i < 3; i++) {
+            String str = sc.next();
+            int x = sc.nextInt();
+            System.out.printf("%-15s", str);
+            System.out.printf("%03d%n", x );
+        }
+        System.out.println("================================");
     }
 }
